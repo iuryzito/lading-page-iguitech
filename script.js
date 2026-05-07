86898f0a430650112235771e0f7dc37333f48833
Iury Lacerda <iurygui544@gmail.com>
2026-05-07 12:36:00 -0300

    adjust landing conversion sections

--- script.js
+++ script.js
@@ -26,18 +26,3 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
     target.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 });
-
-document.querySelectorAll("[data-youtube-id]").forEach((button) => {
-  button.addEventListener("click", () => {
-    const videoId = button.dataset.youtubeId;
-    const iframe = document.createElement("iframe");
-
-    iframe.src = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1`;
-    iframe.title = "Vídeo de apresentação da IGUITECH";
-    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-    iframe.allowFullscreen = true;
-
-    button.replaceChildren(iframe);
-    button.classList.add("is-playing");
-  });
-});
